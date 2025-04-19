735de52da37231cae2d2cece2ffb1b7d8dc4b411
Kalpojyoti Koch <80536379+kalpokoch@users.noreply.github.com>
2025-04-19 22:40:59 +0530

    added business verticals section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
+import OurVerticals from './components/OurVerticals';
 import './index.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Hero />
       <About />
+      <OurVerticals/>
     </div>
   );
 }
