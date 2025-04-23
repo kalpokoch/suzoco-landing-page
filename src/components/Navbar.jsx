5d7033407864a0f385d4f247b6aeb04e1c2f3d69
Kalpojyoti Koch <80536379+kalpokoch@users.noreply.github.com>
2025-04-23 22:15:46 +0530

    hamburger menu added for Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import './Navbar.css';
 
 function Navbar() {
   const [scrolled, setScrolled] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -14,18 +15,29 @@ function Navbar() {
     return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <nav className={`navbar ${scrolled ? 'scrolled' : ''}`}>
       <div className="container">
-        <a href="">
-        <img src={logo} alt="Suzoco Logo" className="logo" />
+        <a href="#">
+          <img src={logo} alt="Suzoco Logo" className="logo" />
         </a>
-        <ul className="nav-links">
+
+        <div className={`nav-links ${menuOpen ? 'open' : ''}`}>
           <li><a href="#about">About</a></li>
           <li><a href="#verticals">Our Verticals</a></li>
           <li><a href="#why-choose-us">Why Choose Us</a></li>
           <li><a href="#contact-us" className='contact-button'>Contact Us</a></li>
-        </ul>
+        </div>
+
+        <div className={`hamburger ${menuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
       </div>
     </nav>
   );
