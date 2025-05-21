af0f184785268ca3e7ab09ebd364dd6c16387f0e
Kalpojyoti Koch <80536379+kalpokoch@users.noreply.github.com>
2025-05-21 22:39:40 +0530

    backend added for contactus.jsx

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebookF, faLinkedinIn, faInstagram } from '@fortawesome/free-brands-svg-icons';
 import { MapPinHouse, Mails, PhoneIncoming } from 'lucide-react';
@@ -6,6 +6,43 @@ import FadeInOnScroll from './animations/FadeInOnScroll';
 import './ContactUs.css';
 
 const ContactUs = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    inquiryType: 'General Inquiry',
+    message: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prev => ({ ...prev, [name]: value}));
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try{
+      const res = await fetch('http://localhost:5000/api/contact', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const result = await res.json();
+      if (res.ok) {
+        alert('message sent successfully');
+        setFormData({name: '', email: '', phone: '', inquiryType: 'General Inquiry', message: ''})
+      } else{
+        alert('Failed to send message');
+      }
+    } catch (err){
+      console.error(err);
+      alert('Error occured. Please try again later.');
+    }
+  };
+
   return (
     <section id= 'contact-us' className="contact-section">
       <FadeInOnScroll>
@@ -15,19 +52,46 @@ const ContactUs = () => {
           Get in touch with us today.</p>
 
         <div className="contact-container">
-          <div className="contact-form">
-            <input type="text" placeholder="Your Name" />
-            <input type="email" placeholder="Email Address" />
-            <input type="tel" placeholder="Phone Number" />
-            <select>
+          <form className="contact-form" onSubmit={handleSubmit}>
+            <input 
+              type="text"
+              name="name"
+              placeholder="Your Name"
+              value={formData.name}
+              onChange={handleChange}
+            />
+            <input
+              type="email"
+              name="email"
+              placeholder="Email Address"
+              value={formData.email}
+              onChange={handleChange}
+            />
+            <input
+              type="tel"
+              name='phone'
+              placeholder="Phone Number"
+              value={formData.phone}
+              onChange={handleChange}
+            />
+            <select
+              name='inquiryType'
+              value={formData.inquiryType}
+              onChange={handleChange}
+            >
               <option>General Inquiry</option>
               <option>Zynco B2B - MarketPlace</option>
               <option>ZCS Asia - Digital Services</option>
               <option>Setu UpSkills - Training</option>
             </select>
-            <textarea placeholder="How Can I help you?"></textarea>
+            <textarea
+              name='message'
+              placeholder='How Can I help You?'
+              value={formData.message}
+              onChange={handleChange}
+            />
             <button type="submit">Send Message</button>
-          </div>
+          </form>
 
           <div className="contact-info">
             <h4>Get in Touch</h4>
